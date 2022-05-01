9e2c1354295e4a9736e19203dfbca8447e87c061
Brice Duke <bricewduke@gmail.com>
2022-05-01 18:17:45 -0500

    feat(api-gateway): add health check module

--- apps/api-gateway/src/app/app.controller.ts
+++ apps/api-gateway/src/app/app.controller.ts
@@ -1,13 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
 
 import { AppService } from './app.service';
+import { globalPrefix, port } from './constants';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly health: HealthCheckService,
+    private readonly http: HttpHealthIndicator
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('health')
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      async () =>
+        this.http.pingCheck(
+          'api-gateway',
+          `http://localhost:${port}/${globalPrefix}`
+        ),
+    ]);
   }
 }
--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,10 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TerminusModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })

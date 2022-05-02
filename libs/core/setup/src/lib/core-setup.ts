1e151993d10c83e54ad8a94f051b40dba3237605
Brice Duke <bricewduke@gmail.com>
2022-05-02 18:57:00 -0500

    chore(core): migrate from grpc to kafka

--- libs/core/setup/src/lib/core-setup.ts
+++ libs/core/setup/src/lib/core-setup.ts
@@ -1,6 +1,5 @@
 import { INestApplication, Logger } from '@nestjs/common';
 import { Transport } from '@nestjs/microservices';
-import { join } from 'path';
 
 export function coreSetup(): string {
   return 'core-setup';
@@ -8,23 +7,22 @@ export function coreSetup(): string {
 
 export const createService = async (
   app: INestApplication,
-  proto: string,
   host = '0.0.0.0',
   port: number,
   serviceName: string
 ) => {
   app.connectMicroservice({
-    transport: Transport.GRPC,
+    transport: Transport.KAFKA,
     options: {
-      url: `${host}:${port}`,
-      package: serviceName,
-      protoPath: join(process.cwd(), `/dist/libs/proto-schema/${proto}`),
+      client: {
+        brokers: [`${host}:${port}`],
+      },
     },
   });
 
   await app.startAllMicroservices();
   await app.listen(null);
 
-  Logger.log(`gRPC service-${serviceName} :: running on port ${port}`);
+  Logger.log(`KAFKA service-${serviceName} :: running on port ${port}`);
   Logger.log(`REST service-${serviceName} :: running at ${await app.getUrl()}`);
 };
